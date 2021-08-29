eb99acd6799b65be00d83ad0dc31dd9372ac3b34
Saikat Safa <56374357+SaikatSafa@users.noreply.github.com>
2021-08-29 02:10:46 -0400

    Add files via upload

---
+++ Module_6_Programming2/objects.js
@@ -0,0 +1,22 @@
+let car = {
+  carBrand: "Lexus",
+  carModel: "GX",
+  carYear: "2021",
+  carColor: "Atomic Silver",
+  get brand() {
+    return this.carBrand;
+  }
+  set color(color) {
+    this carColor = color;
+  }
+};
+
+
+
+let sportsTeam = {
+
+}
+
+let animal = {
+
+}
